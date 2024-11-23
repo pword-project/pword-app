c94bd771ca5f137668b5090537b2bc102cca753a
Carlos Jiménez <cajimenezca@unal.edu.co>
2024-11-23 09:31:11 -0500

    ✨ ADD INTEGRATION W/ SUPABASE AND BASIC AUTH FLOW

---
+++ app.config.ts
@@ -0,0 +1,52 @@
+module.exports = {
+  name: "pword-app",
+  slug: "pword-app",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    package: "com.pword.pwordapp",
+  },
+  web: {
+    bundler: "metro",
+    output: "single",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    router: {
+      origin: false,
+    },
+  },
+  updates: {
+    url: "https://u.expo.dev/5e876ff8-692c-4692-8597-77ad135fea58",
+  },
+  runtimeVersion: {
+    policy: "appVersion",
+  },
+  owner: "pword",
+};
