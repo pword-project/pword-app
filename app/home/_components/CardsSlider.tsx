d14f811ab732b794ffaafe2db03065f1a9297d2a
Carlos Jiménez <cajimenezca@unal.edu.co>
2025-02-23 15:13:20 -0500

    ✨ PRACTICE FEATURE

--- app/home/_components/CardsSlider.tsx
+++ app/home/_components/CardsSlider.tsx
@@ -3,81 +3,79 @@ import infoCardStyles from "@/styles/InfoCard";
 import { useRouter } from "expo-router";
 import { ScrollView } from "react-native";
 import { Dimensions } from "react-native";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
+import { ThemedText } from "@/components/ThemedText";
 
 export default function CardsSlider() {
   const router = useRouter();
   const scrollViewRef = useRef<ScrollView>(null);
-  const scrollPosition = useRef(0);
-
-  useEffect(() => {
-    const intervalId = window.setInterval(() => {
-      if (scrollViewRef.current) {
-        const screenWidth = Dimensions.get("window").width;
-        scrollPosition.current += screenWidth * 0.5;
-
-        if (scrollPosition.current >= screenWidth * 1.5) {
-          scrollPosition.current = 0;
-        }
-
-        scrollViewRef.current.scrollTo({
-          x: scrollPosition.current,
-          animated: true,
-        });
-      }
-    }, 12000);
-
-    return () => window.clearInterval(intervalId);
-  }, []);
 
   return (
-    <ScrollView
-      horizontal={true}
-      style={{
-        marginBottom: 16,
-      }}
-      ref={scrollViewRef}
-    >
-      <InfoCard
-        title="Practice"
-        paragraph="Practice makes perfect! Click the button below to start your training and improve your skills."
-        buttonText="Get started"
-        onPress={() => {
-          router.push("/practice");
+    <>
+      <ThemedText
+        type="title"
+        style={{
+          color: "white",
+          textAlign: "left",
         }}
-        customStyles={{
-          ...infoCardStyles,
-          container: {
-            ...infoCardStyles.container,
-            maxWidth: Dimensions.get("window").width * 0.8,
-          },
+      >
+        Explore Features
+      </ThemedText>
+      <ScrollView
+        horizontal={true}
+        style={{
+          marginBottom: 16,
         }}
-      />
+        ref={scrollViewRef}
+      >
+        <InfoCard
+          title="Practice"
+          paragraph="Practice makes perfect! Click the button below to start your training and improve your skills."
+          buttonText="Get started"
+          onPress={() => {
+            router.push("/practice");
+          }}
+          customStyles={{
+            ...infoCardStyles,
+            container: {
+              ...infoCardStyles.container,
+              maxWidth: Dimensions.get("window").width * 0.8,
+            },
+          }}
+        />
 
-      <InfoCard
-        title="AI"
-        paragraph="AI-powered training! Click the button below to start your training and improve your skills."
-        buttonText="Go to chat"
-        onPress={() => {
-          router.push("/ai");
-        }}
-        customStyles={{
-          ...infoCardStyles,
-          container: {
-            ...infoCardStyles.container,
-            maxWidth: Dimensions.get("window").width * 0.8,
-          },
-        }}
-      />
+        <InfoCard
+          title="AI"
+          paragraph="AI-powered training! Click the button below to start your training and improve your skills."
+          buttonText="Go to chat"
+          onPress={() => {
+            router.push("/ai");
+          }}
+          customStyles={{
+            ...infoCardStyles,
+            container: {
+              ...infoCardStyles.container,
+              maxWidth: Dimensions.get("window").width * 0.8,
+            },
+          }}
+        />
 
-      <InfoCard
-        title="About"
-        paragraph="Learn more about our app and its features."
-        buttonText="Learn more"
-        onPress={() => {
-          router.push("/about");
-        }}
-      />
-    </ScrollView>
+        <InfoCard
+          title="About"
+          paragraph="Learn more about our app and its features."
+          buttonText="Learn more"
+          onPress={() => {
+            router.push("/about");
+          }}
+          customStyles={{
+            ...infoCardStyles,
+            container: {
+              ...infoCardStyles.container,
+              maxWidth: Dimensions.get("window").width * 0.8,
+            },
+          }}
+        />
+      </ScrollView>
+    </>
   );
 }
--- app/practice/translate/index.tsx
+++ app/practice/translate/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
 import { useFlashcards } from "@/contexts/FlashcardsContext";
 import { Flashcard } from "@/types/Flashcard";
@@ -8,58 +8,172 @@ import { ThemedText } from "@/components/ThemedText";
 import {
   StyleSheet,
   TextInput,
-  ScrollView,
   TouchableOpacity,
   Pressable,
+  Platform,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { toast } from "@backpackapp-io/react-native-toast";
+import CollectionsGrid from "@/components/CollectionsGrid";
 
 const FlashcardPractice = () => {
-  const { flashcards } = useFlashcards();
+  const { flashcards, collections, collection, setCollection } =
+    useFlashcards();
+  const logoColor = useThemeColor({}, "primaryLogoBlue");
+  const errorColor = useThemeColor({}, "error");
+  const [currentFlashcard, setCurrentFlashcard] = useState<Flashcard | null>(
+    null,
+  );
+  const [flashcardsPracticed, setFlashcardsPracticed] = useState<Flashcard[]>(
+    [],
+  );
 
   const formik = useFormik({
-    initialValues: flashcards.reduce(
-      (acc: { [key: string]: string }, flashcard: Flashcard) => {
-        acc[flashcard.id] = "";
-        return acc;
-      },
-      {},
-    ),
+    initialValues: { definition: "" },
     onSubmit: (values) => {
-      alert("Submitted values: " + JSON.stringify(values));
+      if (
+        currentFlashcard &&
+        values.definition.toLowerCase() ===
+          currentFlashcard.definition.toLowerCase()
+      ) {
+        setFlashcardsPracticed([...flashcardsPracticed, currentFlashcard]);
+        if (flashcardsPracticed.length + 1 === flashcards.length) {
+          setFlashcardsPracticed([]);
+        }
+        setCurrentFlashcard(
+          flashcards[Math.floor(Math.random() * flashcards.length)],
+        );
+      } else {
+        if (Platform.OS === "web") {
+          toast.error("Incorrect definition. Try again!");
+        } else {
+          alert("Incorrect definition. Try again!");
+        }
+      }
+      formik.resetForm();
     },
   });
 
+  const handleSkip = () => {
+    setCurrentFlashcard(
+      flashcards[Math.floor(Math.random() * flashcards.length)],
+    );
+  };
+
+  useEffect(() => {
+    setCurrentFlashcard(
+      flashcards[Math.floor(Math.random() * flashcards.length)],
+    );
+  }, [flashcards]);
+
   return (
     <Screen>
-      <ScrollView contentContainerStyle={styles.container} horizontal>
-        {flashcards.map((flashcard: Flashcard) => (
-          <ThemedView key={flashcard.id} style={styles.flashcardContainer}>
-            <ThemedText style={styles.flashcardText}>
-              {flashcard.word}
-            </ThemedText>
-            <ThemedText style={styles.labelText}>Definition:</ThemedText>
-            <TextInput
-              style={styles.input}
-              id={flashcard.id}
-              onChangeText={formik.handleChange(flashcard.id)}
-              value={formik.values[flashcard.id]}
-            />
-            <TouchableOpacity
-              style={styles.hintButton}
-              onPress={() => alert(flashcard.definition)}
-            >
-              <Ionicons name="information" size={24} color="black" />
-            </TouchableOpacity>
-          </ThemedView>
-        ))}
-      </ScrollView>
+      <ThemedText type="title">Translate</ThemedText>
+      <ThemedText type="default">
+        Type the definition of the word you see below. Make sure to be as
+        accurate as possible, and remember that spelling counts. This exercise
+        will help you reinforce your memory and understanding of the vocabulary.
+      </ThemedText>
+
+      <ThemedText
+        type="smallprint"
+        style={{
+          marginTop: 16,
+          color: "#666666",
+        }}
+      >
+        If you are unsure, you can always use the hint button to see the correct
+        definition, but try your best to recall it on your own first.
+      </ThemedText>
+
+      <ThemedText
+        type="subtitle"
+        style={{
+          marginVertical: 16,
+        }}
+      >
+        Filter cards by collection
+      </ThemedText>
+      <CollectionsGrid
+        collection={collection}
+        collections={collections}
+        handlePress={(col) => {
+          const newCollection = collection?.id === col.id ? null : col;
+          setCollection(newCollection);
+        }}
+      />
+
+      {flashcards.length > 0 && (
+        <ThemedView style={styles.flashcardContainer}>
+          {currentFlashcard && (
+            <>
+              <ThemedText
+                type="subtitle"
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 24,
+                  marginBottom: 16,
+                  color: "#333",
+                }}
+              >
+                The word is
+              </ThemedText>
+              <ThemedText style={styles.flashcardText}>
+                "{currentFlashcard.word}"
+              </ThemedText>
+              <ThemedText style={styles.labelText}>
+                Write the definition of the word
+              </ThemedText>
+              <TextInput
+                style={styles.input}
+                onChangeText={formik.handleChange("definition")}
+                value={formik.values.definition}
+              />
+              <TouchableOpacity
+                style={styles.hintButton}
+                onPress={() => {
+                  if (Platform.OS === "web") alert(currentFlashcard.definition);
+                  else toast(currentFlashcard.definition);
+                }}
+              >
+                <Ionicons
+                  name="information-circle"
+                  size={24}
+                  color={logoColor}
+                />
+              </TouchableOpacity>
+            </>
+          )}
+        </ThemedView>
+      )}
+
+      {!flashcards.length && (
+        <ThemedView style={styles.flashcardContainer}>
+          <ThemedText style={styles.flashcardText}>
+            No flashcards available
+          </ThemedText>
+        </ThemedView>
+      )}
 
       <Pressable onPress={() => formik.handleSubmit()}>
         <ThemedText type="button" style={styles.submitText}>
           Submit
         </ThemedText>
       </Pressable>
+
+      <Pressable onPress={handleSkip}>
+        <ThemedText
+          type="button"
+          style={{ ...styles.skipText, backgroundColor: errorColor }}
+        >
+          Skip
+        </ThemedText>
+      </Pressable>
+
+      <ThemedText style={styles.progressText}>
+        Words translated: {flashcardsPracticed.length}/{flashcards.length}
+      </ThemedText>
     </Screen>
   );
 };
@@ -74,7 +188,8 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   flashcardContainer: {
-    marginBottom: 15,
+    marginTop: 16,
+    marginBottom: 16,
     flexDirection: "column",
     alignItems: "center",
     marginHorizontal: 10,
@@ -86,17 +201,19 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 5,
     elevation: 3,
-    width: 250,
   },
   flashcardText: {
     fontSize: 18,
     marginBottom: 5,
+    fontStyle: "italic",
     color: "#333",
   },
   labelText: {
     fontSize: 14,
+    marginTop: 10,
     marginBottom: 5,
     color: "#666",
+    alignSelf: "flex-start",
   },
   input: {
     height: 40,
@@ -111,9 +228,20 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   submitText: {
-    color: "#007AFF",
+    color: "#FFF",
     fontSize: 16,
     fontWeight: "bold",
     marginTop: 20,
+  },
+  skipText: {
+    color: "#FFF",
+    fontSize: 16,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  progressText: {
+    marginTop: 20,
+    fontSize: 16,
+    color: "#666666",
   },
 });
